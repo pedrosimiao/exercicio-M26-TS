d79128fcae69fa8e67a979509574c459eb7dd2ef
pedrosimiao <pedrolumiere@gmail.com>
2024-03-15 20:44:17 -0300

    Funções TypeScript

---
+++ exercicio_typescript_ebac.ts
@@ -0,0 +1,10 @@
+function multiplicador(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá, ${nome}!`
+}
+
+console.log(multiplicador(65, 12));
+console.log(saudacao('Pedro'));
